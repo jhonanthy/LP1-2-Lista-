90a3acfa8359d6a95c83ed6b62c19b26297984e0
jhonanthy <52537726+jhonanthy@users.noreply.github.com>
2019-09-01 22:55:32 -0300

    Questão 2

--- main.cpp
+++ main.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
-
-#include "Triangulo.h"
-#include "Quadrado.h"
-#include "Circulo.h"
+#include "ContaEspecial.h"
 
 using namespace std;
 
-int main()
-{
-    FigurasGeometricas *t1 = new Triangulo(5,7);
-    FigurasGeometricas *q1 = new Quadrado(6);
-    FigurasGeometricas *c1 = new Circulo(5);
+int main(){
+    Conta *c1 = new Conta();
+    ContaEspecial *ce1 = new ContaEspecial();
 
-    cout << "Nome da figura: " << t1->getNome() << "\nArea: " << t1->CalcularArea() << "m^2" << endl << endl;
-    cout << "Nome da figura: " << q1->getNome() << "\nArea: " << q1->CalcularArea() << "m^2" << endl << endl;
-    cout << "Nome da figura: " << c1->getNome() << "\nArea: " << c1->CalcularArea() << "m^2" << endl;
+    c1->Depositar(3000);
+    ce1->Depositar(4000);
+
+    cout << "Saldo disponivel da conta num 1: " << c1->getSaldo() << endl;
+    cout << "Saldo disponivel da conta especial num1: " << ce1->getSaldo() << endl;
+
+    c1->Sacar(1500);
+    ce1->Sacar(2200);
+
+    cout << "Saldo disponivel da conta num1 (pos saque): " << c1->getSaldo() << endl;
+    cout << "Saldo disponivel da conta especial num1 (pos saque): " << ce1->getSaldo() << endl;
+
+    c1->setSalarioMensal(3000);
+    c1->DefinirLimite();
+
+    ce1->setSalarioMensal(3000);
+    ce1->DefinirLimite();
+
+    cout << "Limite da conta num1: " << c1->getLimite() << endl;
+    cout << "Limite da conta especial num1: " << ce1->getLimite() << endl;
 
     return 0;
 }
